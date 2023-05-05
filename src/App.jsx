81269256c5b2d1fecd66bafc396284b6503859ad
Anton Trifonov <toha62@inbox.ru>
2023-05-05 16:45:49 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function Menu() {
+  return null;
+}
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" component={DriftPage} />
+          <Route path="/timeattack" component={TimeAttackPage} />
+          <Route path="/forza" component={ForzaPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
